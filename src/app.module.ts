df3e0e68d8b2fe593a4e6da759b1b4ad4a722b6a
Azutech <azunna.onugha@gmail.com>
2024-01-24 18:05:25 +0100

    The environment created

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { BlogspotModule } from './blogspot/blogspot.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/BlogPost'), UsersModule, BlogspotModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/BlogPost'), UsersModule, BlogspotModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
